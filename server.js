1511385d26632870c3a38da0016da557a521387f
aditya0x80 <adityashrest.shrest34@gmail.com>
2022-01-21 22:18:12 +0530

    fix

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const app = express();
 
 const { nanoid } = require("nanoid");
 app.use(express.static("public"));
-
+const cors = require("cors");
 app.set("view engine", "ejs");
 connectDB = require("./config/db");
 connectDB();
+
+const corsOptions = {
+  origin: process.env.ALLOW_CLIENTS.split(","),
+};
+// middleware
+app.use(cors(corsOptions));
 
 //routes
 app.use("/api/mail", require("./routes/files"));
@@ -19,5 +25,3 @@ app.use("/mail/d", require("./routes/download"));
 app.listen(process.env.PORT || 2000, function () {
   console.log("server is running on port 2000");
 });
-
-
